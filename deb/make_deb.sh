bb5881ecbfac7aee57eac8d8667627b9d10794a9
James Harwood <41258460+jamesharwood@users.noreply.github.com>
2018-07-15 17:19:39 +0100

    deb file updated description

--- deb/make_deb.sh
+++ deb/make_deb.sh
@@ -4,9 +4,9 @@
 version=$(cat ../version | tr -d "\r" | tr -d "\n")
 echo "Package: comedi-k8055-bash-interface" > comedi-K8055-bash-interface/DEBIAN/control
 echo "Version: $version" >> comedi-K8055-bash-interface/DEBIAN/control
-echo "Maintainer: James.Harwood@gmail.com" >> comedi-K8055-bash-interface/DEBIAN/control
+echo "Maintainer: James Harwood" >> comedi-K8055-bash-interface/DEBIAN/control
 echo "Architecture: i386" >> comedi-K8055-bash-interface/DEBIAN/control
-echo "Description: Command line utilities to read/write Velleman k8055 cards from bash using comedi kernel drivers." >> comedi-K8055-bash-interface/DEBIAN/control
+echo "Description: Command line utilities to read/write Velleman k8055 cards from bash using comedi kernel drivers. https://github.com/jamesharwood/comedi-K8055_bash_interface" >> comedi-K8055-bash-interface/DEBIAN/control
 
 # copy up to date compiled programs to deb build dir
 cp ../bin/* ./comedi-K8055-bash-interface/usr/local/bin
