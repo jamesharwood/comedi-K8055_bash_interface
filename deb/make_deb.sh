051cc99c738913cec842da55ad8f26c118e972b0
James Harwood <james.harwood@gmail.com>
2018-12-03 18:20:54 +0000

    Add support to compile on amd64 arch and better naming format of output deb files

--- deb/make_deb.sh
+++ deb/make_deb.sh
@@ -2,10 +2,18 @@
 
 # update version number in deb control file
 version=$(cat ../version | tr -d "\r" | tr -d "\n")
+echo $version
 echo "Package: comedi-k8055-bash-interface" > comedi-K8055-bash-interface/DEBIAN/control
 echo "Version: $version" >> comedi-K8055-bash-interface/DEBIAN/control
 echo "Maintainer: James Harwood" >> comedi-K8055-bash-interface/DEBIAN/control
-echo "Architecture: i386" >> comedi-K8055-bash-interface/DEBIAN/control
+if [ $(uname -r | grep -c "amd64" | tr -d "\r" | tr -d "\n") -eq 0 ]; then
+ echo "Architecture: i386" >> comedi-K8055-bash-interface/DEBIAN/control
+ dr="i386"
+else
+ echo "Architecture: amd64" >> comedi-K8055-bash-interface/DEBIAN/control
+ dr="amd64"
+fi
+echo "Depends: libcomedi0 | libcomedi-dev" >> comedi-K8055-bash-interface/DEBIAN/control
 echo "Description: Command line utilities to read/write Velleman k8055 cards from bash using comedi kernel drivers. https://github.com/jamesharwood/comedi-K8055_bash_interface" >> comedi-K8055-bash-interface/DEBIAN/control
 
 # copy up to date compiled programs to deb build dir
@@ -14,4 +22,6 @@ rm ./comedi-K8055-bash-interface/usr/local/bin/*.sh
 cp ../bin/*.sh ./comedi-K8055-bash-interface/usr/share/doc/comedi-K8055_bash_interface/examples
 
 # make deb file
-dpkg-deb --build comedi-K8055-bash-interface
+dpkg-deb --build comedi-K8055-bash-interface comedi-K8055-bash-interface_$version-$dr.deb
+
+
--- version.h
+++ version.h
@@ -1,1 +1,1 @@
-#define code_version 0.35
+#define code_version 0.41
